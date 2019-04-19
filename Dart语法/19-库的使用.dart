0c4e0d9235b9b59e97deb9fe6416194741a7389b
CoderTitan <quanjunt@163.com>
2019-04-19 17:23:46 +0800

    import useing

--- Dart语法/19-库的使用.dart
+++ Dart语法/19-库的使用.dart
@@ -8,7 +8,8 @@ import 'dart:io';
 main() {
 
   // 建立一个文件对象
-  var file = new File('./README.md');
+  var file = new File('17-interface.dart');
   var content = file.readAsStringSync();
   print(content);
 }
+
